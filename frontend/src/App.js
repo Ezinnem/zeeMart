d8eef79749436cafb197a7a788fe880fec02800f
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2020-06-16 03:33:51 +0100

    Created the product page of the uI

---
+++ frontend/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+import data from './data';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import HomeScreen from './Screen/HomeScreen';
+import ProductScreen from './Screen/ProductScreen';
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+    <header className="header">
+        <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Zeemart</Link>
+            {/* <a href="index.html">Zeemart</a> */}
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+            
+        </div>
+    </header>
+    <aside className="sidebar">
+        <h3> Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+            <li><a href="index.html">Suits</a></li>
+            <li><a href="index.html">Gowns</a></li>
+            <li><a href="index.html">Skirts</a></li>
+        </ul>
+ 
+    </aside>
+    <main  className="main">
+        <div className="content">
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" exact={true} component={HomeScreen} />
+         
+        </div>
+ </main>
+ <footer  className="footer">
+     All rights reserved.
+ </footer>
+ </div>
+ </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,69 @@
+export default {
+    products: [
+    {
+        _id:'1',
+        name: 'Suit',
+        category: 'Suits',
+        image: '/images/image1.jpg',
+        price: 200,
+        brand: 'Gucci',
+        rating: 4.5,
+        numReviews: 15,
+        description: "Pant suits for elegance Ladies"
+    },
+    {
+        _id:'2',
+        name: 'Ball Gown',
+        category: 'Gowns',
+        image: '/images/gown.jpg',
+        price: 150,
+        brand: 'Gucci',
+        rating: 4.5,
+        numReviews: 15,
+        description: "Ball gown for Ladies"
+    },
+    {
+        _id:'3',
+        name: 'Wedding Gown',
+        category: 'Gowns',
+        image: '/images/gown2.webp',
+        price: 300,
+        brand: 'Gucci',
+        rating: 4.5,
+        numReviews: 15,
+        description: "Memorable expreience is created for elegance Ladies"
+    },
+    {
+        _id:'4',
+        name: 'Smart Gown',
+        category: 'Gowns',
+        image: '/images/officegown.jpeg',
+        price: 85,
+        brand: 'Gucci',
+        rating: 4.0,
+        numReviews: 20,
+        description: "Brings out the boss Lady in you"
+    },
+    {
+        _id:'5',
+        name: 'Office Gown',
+        category: 'Gowns',
+        image: '/images/officegown2.jpeg',
+        price: 85,
+        brand: 'Gucci',
+        rating: 4.0,
+        numReviews: 20,
+        description: "Makes the official appearance like never before"
+    },
+    {
+        _id:'6',
+        name: 'Fit Skirt',
+        category: 'Skirts',
+        image: '/images/skirt.jpeg',
+        price: 60,
+        brand: 'Gucci',
+        rating: 3.5,
+        numReviews: 12,
+        description: "Perfect feminine look"
+    },]
+}
